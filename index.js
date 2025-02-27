46567b72c3ac066040a45355197bed5a1d749e80
Serhii Parovchenko <serhii.parovchenko@venngo.com>
2025-02-27 11:36:21 +0200

    third commit

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
 require('dotenv').config()
 
-console.log(`${process.env.F_NAME} ${process.env.L_NAME}`)
-console.log(process.env.JOB_TITLE)
+console.log(`${process.env.F_NAME} ${process.env.L_NAME} - ${process.env.JOB_TITLE}`)
